66b8eea7e6fba830e6d2fc9c737ebb47b48ca4e9
David Newman <david.newman@wvumedicine.org>
2020-01-30 19:58:36 -0500

    Code clean Up

--- NewmanAssignment1/Form1.cs
+++ NewmanAssignment1/Form1.cs
@@ -8,14 +8,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NewmanAssignment1.Helpers;
 
 namespace NewmanAssignment1
 {
     public partial class Form1 : Form
     {
 
-        private OpenFileDialog openFileDialog1;
-        private static List<string>  _questions;
+        public static OpenFileDialog openFileDialog1;
+        public static List<string>  _questions;
 
 
         public Form1()
@@ -36,7 +37,7 @@ namespace NewmanAssignment1
         }
 
 
-        private void button3_Click(object sender, EventArgs e)
+        public void button3_Click(object sender, EventArgs e)
         {
             var res = string.Empty;
             
@@ -44,67 +45,7 @@ namespace NewmanAssignment1
             openFileDialog1 = new OpenFileDialog();
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
-                try
-                {
-                    using (var reader = new StreamReader(openFileDialog1.FileName))
-                    {
-                        _questions = new List<string>();
-
-                        var textInBetween = new List<string>();
-
-                        bool startTagFound = false;
-
-
-                        while (!reader.EndOfStream)
-                        {
-                            var line = reader.ReadLine();
-                            if (String.IsNullOrEmpty(line))
-                            {
-                                continue;
-                            }
-                            if (!startTagFound)
-                            {
-                                startTagFound = line.StartsWith("@QUESTIONS");
-                                _questions.Add(line);
-
-                                continue;
-
-                            }
-
-
-                            bool endTagFound = line.StartsWith("@END");
-                            if (endTagFound)
-                            {
-                                // Do stuff with the text you've read in between here
-                                // ...
-                               
-                                textInBetween.Clear();
-                                continue;
-                            }
-
-                            textInBetween.Add(line);
-                            _questions.Add(line);
-
-                            
-
-
-                        }
-                        string result = string.Join(" ", _questions.ToArray());
-                        int pFrom = result.IndexOf("@QUESTIONS") + "@QUESTIONS".Length;
-                        int pTo = result.IndexOf("@ANSWERS");
-
-                        string pResult = result.Substring(pFrom, pTo - pFrom);
-
-                        Console.WriteLine("");
-
-                    }
-
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show($"Security error.\n\nError message: {ex.Message}\n\n" +
-                    $"Details:\n\n{ex.StackTrace}");
-                }
+                FileProcessor.ReadFile();
             }
         }
 
---
+++ NewmanAssignment1/Helpers/FileProcessor.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NewmanAssignment1.Helpers
+{
+   public partial class FileProcessor
+    {
+    
+        public static void ReadFile()
+        {
+            try
+            {
+                using (var reader = new StreamReader(Form1.openFileDialog1.FileName))
+                {
+                    Form1._questions = new List<string>();
+
+                    var textInBetween = new List<string>();
+
+                    bool startTagFound = false;
+
+
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        if (String.IsNullOrEmpty(line))
+                        {
+                            continue;
+                        }
+                        if (!startTagFound)
+                        {
+                            startTagFound = line.StartsWith("@QUESTIONS");
+                            Form1._questions.Add(line);
+
+                            continue;
+
+                        }
+
+
+                        bool endTagFound = line.StartsWith("@END");
+                        if (endTagFound)
+                        {
+                            // Do stuff with the text you've read in between here
+                            // ...
+
+                            textInBetween.Clear();
+                            continue;
+                        }
+
+                        textInBetween.Add(line);
+                        Form1._questions.Add(line);
+
+
+
+
+                    }
+                    string result = string.Join(" ", Form1._questions.ToArray());
+                    int pFrom = result.IndexOf("@QUESTIONS") + "@QUESTIONS".Length;
+                    int pTo = result.IndexOf("@ANSWERS");
+
+                    string pResult = result.Substring(pFrom, pTo - pFrom);
+
+                    Console.WriteLine("");
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Security error.\n\nError message: {ex.Message}\n\n" +
+                $"Details:\n\n{ex.StackTrace}");
+            }
+        }
+
+
+
+    }
+
+}
